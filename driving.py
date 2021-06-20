8faf83986e343738c6288816bb5da39a3bfe5767
iljvm <iljvmqq@gmail.com>
2021-06-20 10:48:59 +0800

    add US

--- driving.py
+++ driving.py
@@ -1,16 +1,15 @@
 country = input('請輸入國籍:')
 age = input('請輸入年齡:')
-age = int(age
-	)
+age = int(age)
 if country == '台灣':
-	if age >= 18:
-		print('可以開車')
+    if age >= 18:
+        print('可以開車')
     else:
-    	print('不能開車')
+        print('不能開車')
 elif country =='美國':
-    if age >= 20:
-    	print('可以開車')
+    if age >= 16:
+        print('可以開車')
     else:
         print('不能開車')
 else:
-	print('請遵守各國法律')
+    print('請遵守各國法律')
