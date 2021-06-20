343ba6e725ad3eef0ca2cc3aed6b92ac8fa3e7cb
iljvm <iljvmqq@gmail.com>
2021-06-20 10:13:36 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,16 @@
+country = input('請輸入國籍:')
+age = input('請輸入年齡:')
+age = int(age
+	)
+if country == '台灣':
+	if age >= 18:
+		print('可以開車')
+    else:
+    	print('不能開車')
+elif country =='美國':
+    if age >= 20:
+    	print('可以開車')
+    else:
+        print('不能開車')
+else:
+	print('請遵守各國法律')
